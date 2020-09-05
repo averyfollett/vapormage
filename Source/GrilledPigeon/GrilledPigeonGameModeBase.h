7cc1e5ee3b55bf95f0cd4780a23e4d98eb6de95c
Avery Follett <avery.follett@mymail.champlain.edu>
2020-09-05 10:32:40 -0400

    added Unreal project

---
+++ Source/GrilledPigeon/GrilledPigeonGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GrilledPigeonGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GRILLEDPIGEON_API AGrilledPigeonGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
